f9cac15883b136ba85c241450b8105291f9c35c2
Guillaume Gagné-Labelle <guillaume.gagne-labelle@umontreal.ca>
2023-03-27 20:04:00 -0400

    read csv

---
+++ carbon_emission.py
@@ -0,0 +1,18 @@
+import argparse
+import copy
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+import time
+from codecarbon import EmissionsTracker
+from data.data_utils import getData
+from archs.archs_utils import getModel
+import utils
+import pandas as pd
+
+emissions = pd.read_csv('emissions.csv').to_dict()
+print(emissions.keys())
+print(emissions["energy_consumed"])
