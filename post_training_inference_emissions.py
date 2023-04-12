1de538f4421a54e6fb202047e0bfbaa8cb707479
meria149 <meriadec.finoude@gmail.com>
2023-04-11 23:55:24 -0400

    inference sparse runs

--- post_training_inference_emissions.py
+++ post_training_inference_emissions.py
@@ -9,12 +9,15 @@ parser.add_argument("--arch_type", default="fc1", type=str, help="fc1 | lenet5")
 args = parser.parse_args()
 
 
-logs = ["inference_lt_pp68x3", "inference_lt_pp90x2", "inference_regular_pp0x1"]
+# logs = ["inference_lt_pp68x3", "inference_lt_pp90x2", "inference_regular_pp0x1"]
+logs = ["inference_SPARSE_lt_pp90x2", "inference_SPARSE_regular_pp0x1"]
 seeds = [0, 1, 2, 3, 4]
 metrics = ["duration", "emissions", "emissions_rate", "cpu_power", "gpu_power", "ram_power", "cpu_energy", "gpu_energy",
            "ram_energy", "energy_consumed"]
 y_titles = ["Durée [s]", "Émissions de CO2 [kg]", "Taux d'émissions de CO2 [kg/s]", "Puissance CPU [W]", "Puissance GPU [W]", "Puissance RAM [W]", "Énergie CPU [kWh]", "Énergie GPU [kWh]", "Énergie RAM [kWh]", "Énergie consommée [kWh]"]
-legends = ["Élagage 2x68%", "Élagage 90%", "Sans élagage"]
+# legends = ["Élagage 2x68%", "Élagage 90%", "Sans élagage"]
+legends = ["Élagage 90%", "Sans élagage"]
+
 
 # Plots vs nb_seen_images
 for idx, metric in enumerate(metrics):
@@ -22,7 +25,7 @@ for idx, metric in enumerate(metrics):
     for i, log in enumerate(logs):
         y = []
         for seed in seeds:
-            emissions = pd.read_csv(f"saves/{args.arch_type}/{args.dataset}/inference/{log}_seed{seed}.csv").to_dict()
+            emissions = pd.read_csv(f"saves/{args.arch_type}/{args.dataset}/inference_sparse/{log}_seed{seed}.csv").to_dict()
             if len(y) == 0: x = list(emissions[metric].keys())[:-1]
             y.append(list(emissions[metric].values())[:-1])
         x = np.array(x)*50000
@@ -42,7 +45,7 @@ for idx, metric in enumerate(metrics):
     for i, log in enumerate(logs):
         y = []
         for seed in seeds:
-            emissions = pd.read_csv(f"saves/{args.arch_type}/{args.dataset}/inference/{log}_seed{seed}.csv").to_dict()
+            emissions = pd.read_csv(f"saves/{args.arch_type}/{args.dataset}/inference_sparse/{log}_seed{seed}.csv").to_dict()
             if len(y) == 0: x = list(emissions["duration"].values())[:-1]
             y.append(list(emissions[metric].values())[:-1])
         y = np.array(y)
